affebe48fecbadbe1271bdf1abb2937881d0ef30
Nabeel Khan <43550603+nakhan8@users.noreply.github.com>
2020-03-27 00:08:05 +0530

    built

---
+++ Hawkanddove/Assets/SpawnRandomFood.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnRandomFood : MonoBehaviour
+{
+    public Vector3 center;
+    public GameObject hawkPrefab;
+    public GameObject dovePrefab;
+    public GameObject foodPrefab;
+    public int min;
+    public int max;
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+
+    public void Btn_Start_Click()
+    {
+        for (int i = 0; i < 100; i++)
+        {
+            spawnPrefab(hawkPrefab);
+            spawnPrefab(dovePrefab);
+            spawnPrefab(foodPrefab);
+        }
+    }
+
+    public void spawnPrefab(GameObject prefab)
+    {
+        Vector3 pos = center + new Vector3(Random.Range(min, max), Random.Range(min, max), 0);
+
+        Instantiate(prefab, pos, Quaternion.identity);
+    }
+}
